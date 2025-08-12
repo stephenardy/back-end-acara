4072f49de6a84dd147c2936ae4111a1f5ce3ece6
stephenardy <ardystephen@gmail.com>
2025-08-12 08:30:39 +0700

    fix: property isPublish and location.address

--- src/models/event.model.ts
+++ src/models/event.model.ts
@@ -17,7 +17,13 @@ export const eventDAO = Yup.object({
   createdBy: Yup.string().required(),
   createdAt: Yup.string(),
   updatedAt: Yup.string(),
-  location: Yup.object().required(),
+  location: Yup.object()
+    .shape({
+      region: Yup.number(),
+      coordinates: Yup.array(),
+      address: Yup.string(),
+    })
+    .required(),
 });
 
 export type TEvent = Yup.InferType<typeof eventDAO>;
@@ -84,6 +90,9 @@ const EventSchema = new Schema<Event>(
         coordinates: {
           type: [Schema.Types.Number],
           default: [0, 0],
+        },
+        address: {
+          type: Schema.Types.String,
         },
       },
     },
